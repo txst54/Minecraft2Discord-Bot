264fa96091d7180590ea87ee578ea7751cd148fb
txst54 <33455116+txst54@users.noreply.github.com>
2021-06-15 19:27:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,26 +43,11 @@ async def on_ready():
 	print('Done initializing NGROK')
 	print(f"Running Screens:{sc.list_screens()}")
 	while True:
-		i = 0
 		for line in server.logs:
 			status = [x for x in mc if('<'+x+'>' in line)]
 			if bool(status):
-				print(status)
-				print(f'{status[0]} said a thing!')
 				content = line.split('<'+status[0]+'>')[1][:-4]
-				print(content)
 				sender = await client.fetch_user(uuids[status[0]])
-				print(sender.avatar)
-				async with aiohttp.ClientSession() as session:
-					async with session.get(str(sender.avatar_url)) as r:
-						data = await r.read()
-				print("Edited Bot!")
-				#if(client.user.name != sender.name):
-				#	await client.user.edit(username=sender.name)
-				print("Bot has a name!")
 				await wormhole.send(status[0]+": "+content)
-				path = "sigma.PNG"
-				fp = open(path, 'rb')
-				pfp = fp.read()
 				await client.user.edit(username="MineServer")
 client.run(TOKEN)
