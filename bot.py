9d3199d4e63e44c79e3e9cd149c1b2679fb4be6b
txst54 <33455116+txst54@users.noreply.github.com>
2021-06-15 19:37:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import os
 
-import aiohttp
-import discord
-from dotenv import load_dotenv
-import screenutils as sc
-from pyngrok import ngrok
+# import aiohttp
+import discord # pip install discord
+from dotenv import load_dotenv # pip install python-dotenv
+import screenutils as sc # pip install screenutils
+from pyngrok import ngrok # pip install pyngrok
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -30,8 +30,8 @@ async def on_ready():
 		print('Server session does not exist, initializing...')
 		server.initialize()
 		server.enable_logs()
-		server.send_commands('cd ../server')
-		server.send_commands('./run.bat')
+		server.send_commands('cd ../server') # change path to path of the server file
+		server.send_commands('./run.bat') # change the bat/sh file to whatever bat/sh file starts the server
 		for f in server.logs:
 			if("help" in f):
 				break
@@ -39,9 +39,9 @@ async def on_ready():
 				print(f)
 	print('Done initializing server')
 	ip = ngrok.connect(25565, "tcp")
-	await ip_log.send(ip)
+	await ip_log.send(ip) # sends IP in the IP Logs Channel
 	print('Done initializing NGROK')
-	print(f"Running Screens:{sc.list_screens()}")
+	print(f"Running Screens:{sc.list_screens()}") # output should be at least 1 screen and one of the screens should be called server. Depends on how many other screen sessions you are running. 
 	while True:
 		for line in server.logs:
 			status = [x for x in mc if('<'+x+'>' in line)]
@@ -49,5 +49,4 @@ async def on_ready():
 				content = line.split('<'+status[0]+'>')[1][:-4]
 				# sender = await client.fetch_user(uuids[status[0]])
 				await wormhole.send(status[0]+": "+content)
-				await client.user.edit(username="MineServer")
 client.run(TOKEN)
